1a35edd6e04d5778091fda5eeb467aa89377a913
Aidan Marlin <aidan.marlin@gmail.com>
2016-01-06 21:09:56 +0000

    Bug fix

--- torc_application/torc_web.py
+++ torc_application/torc_web.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
 from flask_wtf import Form, RecaptchaField
 from flask_wtf.file import FileField
-from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+from wtforms import TextField, TextAreaField, HiddenField, ValidationError, RadioField,\
     SelectField, BooleanField, SubmitField, IntegerField, FormField, validators
 from wtforms.validators import Required
 
