3b9eae2fb6ed6add1c04de2757f479812173cf26
Aidan Marlin <aidan.marlin@gmail.com>
2016-06-03 15:02:26 +0000

    Changed docker check

--- torc_application/__init__.py
+++ torc_application/__init__.py
@@ -11,7 +11,7 @@ app = create_app()
 # Don't fetch from the CDN..
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
-if os.path.isfile('/.dockerinit'):
+if os.path.isfile('/.dockerenv'):
     print("Running in docker")
     #app.run(host='0.0.0.0',debug=True,threaded=True,ssl_context=context)
     app.run(host='0.0.0.0',debug=True,threaded=True,port=8080)
